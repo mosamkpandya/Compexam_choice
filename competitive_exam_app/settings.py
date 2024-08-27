5f6a9bb7daa17893c49bf93c590ab6bf7624af9a
mosamkpandya <mosamkpandya@gmail.com>
2024-08-27 20:24:00 +0530

    update

--- competitive_exam_app/settings.py
+++ competitive_exam_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1!ebxov!%g089bn!v!)aot!-f09*xmvv8ux91^40wy2t$qze_o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
